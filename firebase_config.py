95b5ce7fe439f0e7a2bc6b647e17b23dcfc7ab03
Adegbehingbe Oluwakorede Joseph <oluwakorede@Adegbehingbes-MacBook-Pro.local>
2026-03-06 11:57:33 +0100

    complete project

--- firebase_config.py
+++ firebase_config.py
@@ -1,15 +1,13 @@
 from decouple import config
 
-
 firebase_config = {
-    'apiKey': "AIzaSyCUzTFWjsDW816NT8JgNQiCMgmGwUxkGj0",
-    'authDomain': "python-storage-d1e10.firebaseapp.com",
-    'databaseURL': "https://python-storage-d1e10-default-rtdb.firebaseio.com",
-    'projectId': "python-storage-d1e10",
-    'storageBucket': "python-storage-d1e10.appspot.com",
-    'messagingSenderId': "253588525093",
-    'appId': "1:253588525093:web:b983734a4caf1ea32c6fcc",
-    'measurementId': "G-VPQSJXB8W5",
-    'serviceAccount': 'serviceAccount.json' if config("PYTHON_ENV") == "dev" else '/etc/secrets/serviceAccount.json',
-    'databaseURL': "https://python-storage-d1e10-default-rtdb.firebaseio.com"
+    'apiKey': config('FIREBASE_API_KEY'),
+    'authDomain': config('FIREBASE_AUTH_DOMAIN'),
+    'databaseURL': config('FIREBASE_DATABASE_URL'),
+    'projectId': config('FIREBASE_PROJECT_ID'),
+    'storageBucket': config('FIREBASE_STORAGE_BUCKET'),
+    'messagingSenderId': config('FIREBASE_MESSAGING_SENDER_ID'),
+    'appId': config('FIREBASE_APP_ID'),
+    'measurementId': config('FIREBASE_MEASUREMENT_ID'),
+    'serviceAccount': config('FIREBASE_SERVICE_ACCOUNT_DEV') if config('PYTHON_ENV') == 'dev' else config('FIREBASE_SERVICE_ACCOUNT_PROD'),
 }
